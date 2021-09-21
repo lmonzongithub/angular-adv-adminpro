26e3a7119bfa3e2c8d4d12fd73f98fb09c6b87ca
lmonzon <lmonzon@natconsultores.com.ar>
2021-09-20 22:22:39 -0300

    fin seccion 4

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    BreadcrumbsComponent,
+    SidebarComponent
+  ],
+  exports:[HeaderComponent,
+    BreadcrumbsComponent,
+    SidebarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
